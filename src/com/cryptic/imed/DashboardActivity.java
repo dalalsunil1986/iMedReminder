eb892d3b846f6617ff48337a52a181117d4459c1
Sharafat Ibn Mollah Mosharraf <sharafat_8271@yahoo.co.uk>
2012-06-02 11:34:36 +0600

    Initial Commit

---
+++ src/com/cryptic/imed/DashboardActivity.java
@@ -0,0 +1,14 @@
+package com.cryptic.imed;
+
+import android.os.Bundle;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+
+@ContentView(R.layout.dashboard)
+public class DashboardActivity extends RoboActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
